9e44fa6611f3d1ded372b91dd0f3d58b615944ea
ParsaJR <parsajr@proton.me>
2026-06-30 12:51:48 +0330

    api: Better comments

--- app/schemas/management.py
+++ app/schemas/management.py
@@ -8,12 +8,12 @@ class ExpiryDuration():
 
 @dataclass
 class APICapabilities():
-    """Used to inform the clients about the available options."""
+    """Used to inform the clients about the available api capabilities."""
     expiry_durations: list[ExpiryDuration]
 
 @dataclass
 class APIBranding():
-    """Used to inform the clients about the available options."""
+    """Used to inform the clients about the service information e.g. the owner's email"""
     app_name: str
     support_email: str
     app_description: str
